6b657b7d4c7723ddb39ff99a164f451255ae2802
Seeshma Babu <seeshmababu@gmail.com>
2025-02-05 18:33:54 -0500

    Added plot

---
+++ mergeSort/plot_results.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+sizes, times = np.loadtxt('results.txt', unpack=True)
+plt.figure()
+plt.plot(sizes, times, marker='o')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('Array Size (log scale)')
+plt.ylabel('Time (log scale)')
+plt.title('Merge Sort Performance')
+plt.grid(True)
+plt.savefig('mergeSort_plot.png')
+plt.show()
